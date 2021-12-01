15b856f2df4510d5c64a686a17a4ffcd823cc11a
German Babichev <Babichev.G.V@sberbank.ru>
2021-12-01 21:48:18 +0300

    поворот экрана + передвижение + пол + цвета

--- main.cpp
+++ main.cpp
@@ -2,23 +2,48 @@
 #include <cmath>
 #include "SFML/Graphics.hpp"
 
-int main()
-{
-    int w = 1080;
-    int h = 720;
+sf::Vector3f rotY(sf::Vector3f &dir, float angle) {
+    sf::Vector3f res;
+    float cos = std::cos(angle);
+    float sin = std::sin(angle);
+    res.x = dir.x * cos + dir.z * sin;
+    res.y = dir.y;
+    res.z = -dir.x * sin + dir.z  * cos;
+    return res;
+}
+
+sf::Vector3f rotZ(sf::Vector3f &dir, float angle) {
+    sf::Vector3f res;
+    float cos = std::cos(angle);
+    float sin = std::sin(angle);
+    res.x = dir.x * cos - dir.y * sin;
+    res.y = dir.x * sin + dir.y * cos;
+    res.z = dir.z;
+    return res;
+}
+
+int main() {
+    float DOUBLE_PI = 2 * M_PI;
+
+    int w = 1920;
+    int h = 1080;
 
     int mouseX = w / 2;
     int mouseY = h / 2;
+    float mx = 0;
+    float my = 0;
     float mouseSensitivity = 1.0f;
-    bool mouseVisible = true;
+    bool mouseVisible = false;
+
+    float speed = 0.1;
 
 
     sf::Vector3f pos = sf::Vector3f(-5.0f, 0.0f, 0.0f);
-    sf::Vector3f viewDir = sf::Vector3f(1, 0, 0);
 
     sf::RenderWindow window(sf::VideoMode(w, h), "Ray tracing", sf::Style::Titlebar | sf::Style::Close);
     window.setFramerateLimit(60);
     window.setMouseCursorVisible(mouseVisible);
+//    sf::Mouse::setPosition(sf::Vector2i(w / 2, h / 2), window);
 
     sf::RenderTexture texture;
     texture.create(w, h);
@@ -27,6 +52,27 @@ int main()
     sf::Shader shader;
     shader.loadFromFile("shader.glsl", sf::Shader::Fragment);
     shader.setUniform("u_resolution", sf::Vector2f(w, h));
+
+    // text
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf"))
+    {
+        return 1;
+    }
+
+    sf::Text text;
+
+    // select the font
+    text.setFont(font); // font is a sf::Font
+
+    // set the character size
+    text.setCharacterSize(24); // in pixels, not points!
+
+    // set the color
+    text.setFillColor(sf::Color::White);
+
+    // set the text style
+    text.setStyle(sf::Text::Bold);
 
     while (window.isOpen())
     {
@@ -38,27 +84,61 @@ int main()
             if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 window.close();
             } else if (event.type == sf::Event::MouseMoved) {
-                mouseX = event.mouseMove.x;
-                mouseY = event.mouseMove.y;
+                mouseX += event.mouseMove.x - w / 2;
+                mouseY += event.mouseMove.y - h / 2;
+                sf::Mouse::setPosition(sf::Vector2i(w / 2, h / 2), window);
+                mx = ((float)mouseX / w - 0.5f) * mouseSensitivity;
+                my = -((float)mouseY / h - 0.5f) * mouseSensitivity;
+
+                mx -= DOUBLE_PI * ((int)(mx / DOUBLE_PI));
+                my -= DOUBLE_PI * ((int)(my / DOUBLE_PI));
+                my = fmin(M_PI/2, fmax(-M_PI/2, my));
+
             } else if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::W);
-//                if (event.key.code == sf::Keyboard::A) wasdUD[1] = true;
-//                if (event.key.code == sf::Keyboard::S) wasdUD[2] = true;
-//                if (event.key.code == sf::Keyboard::D) wasdUD[3] = true;
-//                if (event.key.code == sf::Keyboard::Space) wasdUD[4] = true;
-//                if (event.key.code == sf::Keyboard::LShift) wasdUD[5] = true;
+                sf::Vector3f flyDir = sf::Vector3f(0, 0, 0);
+
+                if (event.key.code == sf::Keyboard::Num0)
+                    speed += 0.1;
+                else if (event.key.code == sf::Keyboard::Num9)
+                    speed -= 0.1;
+
+                // вперед / назад
+                if (event.key.code == sf::Keyboard::W)
+                    flyDir = sf::Vector3f(1, 0, 0);
+                else if (event.key.code == sf::Keyboard::S)
+                    flyDir = sf::Vector3f(-1, 0, 0);
+
+                 // влево / вправо
+                if (event.key.code == sf::Keyboard::A)
+                    flyDir = sf::Vector3f(0, -1, 0);
+                else if (event.key.code == sf::Keyboard::D)
+                    flyDir = sf::Vector3f(0, 1, 0);
+
+                // вверх / вниз
+                if (event.key.code == sf::Keyboard::Space)
+                    pos.z -= speed;
+                else if (event.key.code == sf::Keyboard::LControl)
+                    pos.z += speed;
+
+                flyDir = rotY(flyDir, my);
+                flyDir = rotZ(flyDir, mx);
+
+                pos += flyDir * speed;
             }
         }
 
         // clear the window with black color
         window.clear(sf::Color::Black);
 
+        // set the string to display
+        text.setString("pos: x " + std::to_string(pos.x) + " y " + std::to_string(pos.y) + " z " + std::to_string(pos.z) + '\n' + "speed: " + std::to_string(speed));
 
-        std::cout << mouseX << ' ' << mouseY << '\n';
-        shader.setUniform("u_mouse", sf::Vector2f(mouseX, mouseY));
+        shader.setUniform("u_mouse", sf::Vector2f(mx, my));
+        shader.setUniform("u_pos", pos);
 
         // draw everything here...
         window.draw(sprite, &shader);
+        window.draw(text);
 
         // end the current frame
         window.display();
--- shader.glsl
+++ shader.glsl
@@ -1,9 +1,29 @@
-#version 130
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
+uniform vec3 u_pos;
 
-vec2 sphIntersect( in vec3 ro, in vec3 rd, in vec3 ce, float ra ) {
+const float MAX_DIST = 99999.0;
+
+struct Sphere {
+    vec3 color;
+    vec3 origin;
+    float radius;
+};
+
+mat3 rotY(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat3(c, 0, s, 0, 1, 0, -s, 0, c);
+}
+
+mat3 rotZ(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat3(c, -s, 0, s, c, 0, 0, 0, 1);
+}
+
+vec2 sphIntersect(in vec3 ro, in vec3 rd, in vec3 ce, float ra) {
     vec3 oc = ro - ce;
     float b = dot( oc, rd );
     float c = dot( oc, oc ) - ra*ra;
@@ -13,18 +33,67 @@ vec2 sphIntersect( in vec3 ro, in vec3 rd, in vec3 ce, float ra ) {
     return vec2( -b-h, -b+h );
 }
 
+// plane degined by p (p.xyz must be normalized)
+float plaIntersect( in vec3 ro, in vec3 rd, in vec4 p )
+{
+    return -(dot(ro,p.xyz)+p.w)/dot(rd,p.xyz);
+}
+
+vec3 castRay(vec3 ro, vec3 rd) {
+    vec2 minIt = vec2(MAX_DIST);
+    vec2 it;
+    vec3 n;
+    vec3 col;
+
+    Sphere sphere = Sphere(vec3(1.0, 0.2, 0.1), vec3(0), 1.0);
+
+    it = sphIntersect(ro, rd, sphere.origin, sphere.radius);
+    if(it.x > 0.0 && it.x < minIt.x) {
+        minIt = it;
+        vec3 itPos = ro + rd * it.x;
+        n = itPos - sphere.origin;
+        col = sphere.color;
+    }
+
+    vec3 planeNormal = vec3(0.0, 0.0, -1.0);
+    it = vec2(plaIntersect(ro, rd, vec4(planeNormal, 1.0)), 0);
+    if(it.x > 0.0 && it.x < minIt.x) {
+        minIt = it;
+        n = planeNormal;
+        col = vec3(0.5);
+    }
+    if(minIt.x == MAX_DIST) return vec3(0.529, 0.808, 0.922);
+    vec3 light = normalize(vec3(-0.5, 0.75, -1.0));
+    float diffuse = max(0.0, dot(light, n)) * 0.5;
+    float specular = pow(max(0.0, dot(reflect(rd, n), light)), 32.0);
+    col *= mix(diffuse, specular, 0.5);
+    return col;
+}
+
 void main() {
-    vec2 uv = gl_TexCoord[0].xy;
-    vec2 mouse = (u_mouse / u_resolution);
-    vec3 color = vec3(0);
-    if (uv.x > mouse.x) {
-        color.r = 1;
-    }
-    if (uv.y > mouse.y) {
-        color.g = 1;
-    }
-    if (distance(uv, mouse) < 0.1) {
-        color = vec3(1,1,1);
-    }
-    gl_FragColor = vec4(color, 1.0);
+    vec2 uv = (gl_TexCoord[0].xy - 0.5) * u_resolution / u_resolution.y;
+//    vec2 mouse = (u_mouse / u_resolution) * u_resolution / u_resolution.y;
+    vec3 color;
+
+    vec3 rayOrigin = u_pos; // позиция камеры
+    vec3 rayDirection = normalize(vec3(1, uv)); // вектор смотрящий вперед на пиксель
+
+    // поворот в зависимости от мыши
+    rayDirection *= rotY(u_mouse.y);
+    rayDirection *= rotZ(u_mouse.x);
+
+    //
+    color = castRay(rayOrigin, rayDirection);
+
+
+//    if (distance(uv, mouse) < 0.01) {
+//        color = vec4(1);
+//    }
+
+    // гамма-коррекция
+    color.x = pow(color.x, 0.45);
+    color.y = pow(color.y, 0.45);
+    color.z = pow(color.z, 0.45);
+
+    gl_FragColor = vec4(color, 1);
 }
